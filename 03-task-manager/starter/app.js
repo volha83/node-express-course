91fa12a6ba33d7012be5cd7a5888528c36ce5a18
Volha Padlipskaya <o.podlipskaya@Mac.lan>
2026-01-07 01:10:32 -0500

    Complete Task Manager API

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -5,20 +5,18 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
 //middleware
 app.use(express.static("./public"));
 app.use(express.json());
 
 //routes
-// app.get("/hello", (req, res) => {
-//   res.send("Task Manaer App");
-// });
 
 app.use("/api/v1/tasks", tasks);
 
 app.use(notFound);
-
-const port = 3000;
+app.use(errorHandlerMiddleware);
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,5 +1,6 @@
 const Task = require("../models/Task");
 const asyncWrapper = require("../middleware/async");
+const { createCustomError } = require("../errors/custom-error");
 //getAll
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({});
@@ -11,11 +12,11 @@ const createTask = asyncWrapper(async (req, res) => {
   res.status(201).json({ task });
 });
 //get
-const getTask = asyncWrapper(async (req, res) => {
+const getTask = asyncWrapper(async (req, res, next) => {
   const { id: taskID } = req.params;
   const task = await Task.findOne({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `no task with id: ${taskID}` });
+    return next(createCustomError(`no task with id: ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
@@ -25,7 +26,7 @@ const deleteTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params;
   const task = await Task.findOneAndDelete({ _id: taskID });
   if (!task) {
-    return res.status(404).json({ msg: `no task with id: ${taskID}` });
+    return next(createCustomError(`no task with id: ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
@@ -38,7 +39,7 @@ const updateTask = asyncWrapper(async (req, res) => {
     runValidators: true,
   });
   if (!task) {
-    return res.status(404).json({ msg: `no task with id: ${taskID}` });
+    return next(createCustomError(`no task with id: ${taskID}`, 404));
   }
   res.status(200).json({ task });
 });
